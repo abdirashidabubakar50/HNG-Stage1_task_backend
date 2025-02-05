4da365059af98e1e1bc9af2e1769628b4fa6c64b
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-02-05 20:49:00 +0300

    🎨 Corrected invalid url error
    corrected invalid url error

--- app.py
+++ app.py
@@ -12,26 +12,29 @@ api = Blueprint('api', __name__, url_prefix='/api')
 @api.route('/classify-number', methods=['GET'])
 def math_fun_fact():
     """ get the number from the url """
+    number = request.args.get('number')
     try:
-        number = int(request.args.get('number'))
+        number = int(number)
     except (TypeError, ValueError):
         alphabet = request.args.get('number')
         return jsonify({
             'number': alphabet,
             'error': True
         }), 400
-    prime = is_prime(number)
-    perfect = is_perfect(number)
-    even_or_odd = is_even_or_odd(number)
-    armstrong = is_armstrong(number)
-    digit_sum = sum_digits(number)
+
+    abs_number = abs(number)
+    prime = is_prime(abs_number)
+    perfect = is_perfect(abs_number)
+    even_or_odd = is_even_or_odd(abs_number)
+    armstrong = is_armstrong(abs_number)
+    digit_sum = sum_digits(abs_number)
 
     properties = []
     if armstrong:
         properties.append("armstrong")
     properties.append(even_or_odd)
 
-    fun_fact = get_number_fact(number)
+    fun_fact = get_number_fact(abs_number)
 
     return jsonify({
         "number": number,
